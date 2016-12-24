f270cb918d5dc49f0de299d659b3bc5bc3ab6386
Chris Walsh <attrition.h@gmail.com>
2016-12-24 01:35:39 -0500

    unnecessary route

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var app = express();
 var template = require('pug').compileFile(__dirname + '/source/templates/homepage.pug');
 var path = require('path');
 
-// check if 'dev' is passed in as an argument and use port 8080 in that case 
+// check if 'dev' is passed in as an argument and use port 8080 in that case
 
 // by default don't include a name, as it would be printed in the footer
 var env = {
@@ -14,7 +14,8 @@ var env = {
 
 if (process.argv.length >= 3) {
     if (process.argv[2] === "dev") {
-        env = { port: 8080, name: "dev" };
+        env.port = 8080;
+        env.name = "dev";
     }
 }
 
@@ -29,13 +30,9 @@ app.get("/", function(req, res) {
     res.send(template({ env: env.name }));
 });
 
-app.get("/projects/ld37/", function(req, res) {
-   res.sendFile(path.join(__dirname, "ld37/index.html"));
-});
-
 // exec
 
-app.listen(env.port, function() { 
+app.listen(env.port, function() {
     console.log("Listening on port " + env.port);
 });
 
